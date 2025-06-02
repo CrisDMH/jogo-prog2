f169a3710be410c0e913d99f41fc9b33e78eb7dc
Cristiano <cris29mh@gmail.com>
2025-06-02 18:59:30 -0300

    Add files via upload

---
+++ A3_prog2/menu.h
@@ -0,0 +1,23 @@
+#ifndef _DISPLAU_
+#define _DISPLAY_
+
+#define INICIAR 0
+#define CONFIGURACOES 1
+#define SAIR 2 
+
+void menu_aparencia(int opcoes, ALLEGRO_DISPLAY *disp, ALLEGRO_BITMAP *imagem);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
